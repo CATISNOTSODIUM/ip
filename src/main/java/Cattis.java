90dd3cb979593289eed049c87422c548980afcf0
Thitiwat Kosolpattanadurong <thitiwatipst@icloud.com>
2025-08-14 00:44:29 +0800

    Rename main class

---
+++ src/main/java/Cattis.java
@@ -0,0 +1,8 @@
+import constants.Constants;
+
+public class Cattis {
+    public static void main(String[] args) {
+        System.out.println(Constants.GREETING_MSG);
+        System.out.println(Constants.EXIT_MSG);
+    }
+}
