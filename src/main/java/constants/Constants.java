1bb73bd86ea05e43d4e948c154a5bb7c9532c9b1
Thitiwat Kosolpattanadurong <thitiwatipst@icloud.com>
2025-08-14 00:39:15 +0800

    Add greeting and exiting messages
    Abstract all constants in Constants.java file.
    Implement basic greeting and exiting messages.

---
+++ src/main/java/constants/Constants.java
@@ -0,0 +1,10 @@
+package constants;
+
+public final class Constants {
+    public static final String CHATBOT_NAME = "CATTIS \uD83D\uDC31";
+    public static final String HORIZONTAL_LINE = "__________";
+
+    public static final String GREETING_MSG = String.format(
+            "Hello! I'm %s.\nWhat can I do for you?", CHATBOT_NAME);
+    public static final String EXIT_MSG = "Bye. Hope to see you again soon!";
+}
