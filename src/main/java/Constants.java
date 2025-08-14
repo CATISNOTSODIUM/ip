88be66390e7b18acb0a8edde4429db9bbed97db3
CATISNOTSODIUM <coffee_cat@DESKTOP-4LOUDBB.localdomain>
2025-08-14 10:41:23 +0800

    Remove trailing space from message

--- src/main/java/Constants.java
+++ src/main/java/Constants.java
@@ -5,12 +5,11 @@ public final class Constants {
     public static final String GREETING_MSG = String.format(
             "Hello! I'm %s.\nWhat can I do for you?", CHATBOT_NAME);
     public static final String EXIT_MSG = "Bye. Hope to see you again soon!";
-    public static final String LIST_TASK_MSG = "Here are the tasks in your list: ";
+    public static final String LIST_TASK_MSG = "Here are the tasks in your list:";
     public static final String MARK_TASK_MSG = "Nice! I've marked this task as done:\n%s\n";
     public static final String UNMARK_TASK_MSG = "OK, I've marked this task as not done yet:\n%s\n";
     public static final String ADD_TASK_MSG = "Got it. I've added this task:\n%s\n";
     public static final String TASK_LIST_SUMMARY = "Now you have %s tasks in the list.\n";
-    public static final String INPUT_DECORATOR = "~ ";
     public static final String ADD_DECORATOR = "added: %s";
     // Commands
     public static final String CMD_LIST = "list";
