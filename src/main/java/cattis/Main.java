f66ebe189b1db096514306b2404747d77b050760
CATISNOTSODIUM <coffee_cat@DESKTOP-4LOUDBB.localdomain>
2025-08-22 17:01:41 +0800

    Init java-fx project

---
+++ src/main/java/cattis/Main.java
@@ -0,0 +1,17 @@
+package cattis;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * Main instance of the application
+ */
+public class Main extends Application {
+
+    private Cattis duke = new Cattis("");
+
+    @Override
+    public void start(Stage stage) {
+        stage.show();
+    }
+}
