46dfe63be11875d4df80e89631a8a315bf2d1483
Thitiwat Kosolpattanadurong <thitiwatipst@icloud.com>
2025-09-13 14:10:16 +0800

    Improve documentation and error messages

--- src/main/java/cattis/Constants.java
+++ src/main/java/cattis/Constants.java
@@ -1,12 +1,12 @@
 package cattis;
 
 /**
- * A collection of all global constants for future configuration.
+ * A collection of all global constants for {@code Cattis} Application
+ * It is important to put all relevant string literals in this
+ * class, except error messages which are handled in {@code CattisException}.
  */
 public final class Constants {
     public static final String CHATBOT_NAME = "CATTIS \uD83D\uDC31";
-    public static final String HORIZONTAL_LINE = "__________";
-
     public static final String GREETING_MSG = String.format(
             "Hello! I'm %s.\nWhat can I do for you?", CHATBOT_NAME);
     public static final String EXIT_MSG = "Bye. Hope to see you again soon!";
@@ -18,16 +18,4 @@ public final class Constants {
     public static final String REMOVE_TASK_MSG = "Noted. I've removed this task:\n%s\n";
     public static final String SUMMARY_TASK_MSG = "Now you have %s tasks in the list.\n";
     public static final String EMPTY_MSG = "<empty>";
-    // Commands
-    public static final String CMD_LIST = "list";
-    public static final String CMD_EXIT = "bye";
-    public static final String CMD_MARK = "mark";
-    public static final String CMD_UNMARK = "unmark";
-    public static final String CMD_DELETE = "delete";
-    // cattis.Task types
-    public static final String CMD_TODO = "todo";
-    public static final String CMD_DEADLINE = "deadline";
-    public static final String CMD_EVENT = "event";
-    // Default
-    public static final String CMD_DEFAULT = "default";
 }
